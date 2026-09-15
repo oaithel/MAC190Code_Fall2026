b0c05dda568294d57e1e72794644fbc79a21c4a9
B306_Instructor <B306_Instructor@lab.lagcc>
2026-09-15 14:07:34 -0400

    Initial commit

---
+++ src/com/mac190/intro/Intro.java
@@ -0,0 +1,20 @@
+package com.mac190.intro;
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Welcome to MAC190, it's going to be fun!");
+        System.out.print("Today is first day"); //next to be displayed on the same line
+        System.out.println("Take it easy");
+
+        // two slashes precede a comment.
+        //Display a diamond shape using stars
+        /*
+                       *
+                    *     *
+                  *         *
+                    *     *
+                       *
+         */
+    }
+
+}

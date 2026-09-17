0edb5260443c8ba50ec6a0e9dd9793e91ba25756
momo <oaithel@yahoo.com>
2026-09-17 14:51:05 -0400

    9/17/2026

--- src/com/mac190/average/Average.java
+++ src/com/mac190/average/Average.java
@@ -1,7 +1,22 @@
 package com.mac190.average;
+
+import java.util.Scanner;
+
 /*
 Write a program that accepts three integers from the user
 and displays their average
  */
 public class Average {
+    public static void main(String[] args) {
+        //create a Scanner
+        Scanner sc = new Scanner(System.in);
+        //ask the user to input three integers
+        System.out.println("Enter three integers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        double average = (double)(num1 + num2 + num3)/3.0;
+        System.out.println("The average of " + num1 + " + " + num2 + " + " + num3 + " is: " + average) ;
+
+    }
 }

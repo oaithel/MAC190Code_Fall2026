75867414d9c2d011e7f00c51b048ad8a2c1976c9
ProfOmar <oaithel@yahoo.com>
2026-09-22 14:34:51 -0400

    9/22/2026

--- src/com/mac190/selection/Temperature.java
+++ src/com/mac190/selection/Temperature.java
@@ -1,4 +1,7 @@
 package com.mac190.selection;
+
+import java.util.Scanner;
+
 /*
 Write a Java program that reads a temperature from the user and
 it's format (F: for fahrenheit and C: for Celcius). Covert the
@@ -8,18 +11,32 @@ temperature to the appropriate one (fah to celcius or celcius to fah)
  */
 public class Temperature {
     public static void main(String[] args) {
+        //create a Scanner
+        Scanner sc = new Scanner(System.in);
         //ask the user for the temperature
         //get the temperature temp
+        System.out.println("Enter the temperature to convert: ");
+        double temperature = sc.nextDouble();
 
         //ask the user which format is the temperature
         //f for fahrenheit and c for celcius
-
+        System.out.println("Press C if the temperature is in Celcius \n" +
+                "Press F if the temperature is in Fahrenheit: ");
         //get the format
+        char format = sc.next().charAt(0);
 
         //check if the format is celcius, then
+        if(Character.toLowerCase(format) == 'c') {
             //convert from celcius to fahrenheit
+            double fah = temperature*(9.0/5.0) + 32;
             //display the result
-        //else
+            System.out.println(temperature + " Celcius is " + fah + " Fahreneit");
+        }else if(Character.toUpperCase(format) == 'F'){
             //do the opposite
+            double celcius = (temperature -32)*(5.0/9.0);
+            System.out.println(temperature + " Fahrenheit is " + celcius + " Celcius");
+        }else{
+            System.out.println("Invalid format");
+        }
     }
 }

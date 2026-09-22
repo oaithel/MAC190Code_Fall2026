fb75e0a1a66cf799cacddf511d8fad9430df49e1
ProfOmar <oaithel@yahoo.com>
2026-09-22 13:49:32 -0400

    9/22/2026

---
+++ src/com/mac190/selection/Temperature.java
@@ -0,0 +1,11 @@
+package com.mac190.selection;
+/*
+Write a Java program that reads a temperature from the user and
+it's format (F: for fahrenheit and C: for Celcius). Covert the
+temperature to the appropriate one (fah to celcius or celcius to fah)
+ Celcius = Fahrenheit - 32 multiplied by 4/9.
+ fah = celcius multiplied by 9/4 add 32.
+ */
+public class Temperature {
+
+}

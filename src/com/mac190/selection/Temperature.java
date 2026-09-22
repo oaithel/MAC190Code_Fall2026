f9bee7747141d6b723eb4149b834d7cb1805ea91
ProfOmar <oaithel@yahoo.com>
2026-09-22 13:57:01 -0400

    9/22/2026

--- src/com/mac190/selection/Temperature.java
+++ src/com/mac190/selection/Temperature.java
@@ -7,5 +7,19 @@ temperature to the appropriate one (fah to celcius or celcius to fah)
  fah = celcius multiplied by 9/4 add 32.
  */
 public class Temperature {
+    public static void main(String[] args) {
+        //ask the user for the temperature
+        //get the temperature temp
 
+        //ask the user which format is the temperature
+        //f for fahrenheit and c for celcius
+
+        //get the format
+
+        //check if the format is celcius, then
+            //convert from celcius to fahrenheit
+            //display the result
+        //else
+            //do the opposite
+    }
 }

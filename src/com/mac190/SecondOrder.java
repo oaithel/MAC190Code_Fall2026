40504118c911c83257a8eccedd4225bcf48ef4e0
ProfOmar <oaithel@yahoo.com>
2026-09-22 14:44:06 -0400

    9/22/2026

---
+++ src/com/mac190/SecondOrder.java
@@ -0,0 +1,30 @@
+package com.mac190;
+/*
+Write a Java program that reads the three coefficients of a
+second order equation a, b, and c (aX^2 + bX + C = 0)
+
+solve the queation as follows:
+Two cases for a
+1- a = 0
+    - there two cases for b
+        - b = 0
+            -there are two cases for C
+                - c = 0 there for there is infinite number of solution
+                - c not 0 there is no solution
+
+        - b not equal to 0
+            - we have a first order equation bx + c = 0 to which the
+            solution is -c/b
+2- a not equal to 0
+    - Compute the dicriminant D = b^2 - 4ac
+    - There are three different cases
+        - D = 0
+            There is one doubler solution X = -b/2a
+        - D > 0, there are two distinct solutions
+                X1 = -b + sqrt(d)/2a,  x2 = -b - sqrt(d)/2a
+        - D < 0 no real solutions.
+
+
+ */
+public class SecondOrder {
+}

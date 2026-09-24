f76a01ed8c36131ac4720264038c1ece63a1bf97
momo <oaithel@yahoo.com>
2026-09-24 14:28:43 -0400

    9/24/2026

--- src/com/mac190/calculator/Calculator.java
+++ src/com/mac190/calculator/Calculator.java
@@ -32,5 +32,36 @@ public class Calculator {
             System.out.println("Invalid operation");
         }
 
+        //The same if-else cases can be implemented using a switch statement
+        //You can switch only o characters and integers (and strings in Java)
+        //you cannot switch on double.
+        switch (op){
+            case '+' :
+            {
+                System.out.println(num1 +  " + " + num2 + " = " + (num1 + num2));
+                break; //this prevents the switch from going to the next case
+            }
+            case '-':
+            {
+                System.out.println(num1 +  " - " + num2 + " = " + (num1 - num2));
+                break;
+            }
+            case '*':
+            {
+                System.out.println(num1 +  " * " + num2 + " = " + (num1 * num2));
+                break;
+            }
+            case '/':
+            {
+                if(num2 == 0){
+                    System.out.println("Trying to divide by 0!");
+                }else{
+                    System.out.println(num1 +  " / " + num2 + " = " + (num1 / num2));
+                }
+                break;
+            }
+            default:
+                System.out.println("Invalid operation");
+        }
     }
 }

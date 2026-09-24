54ddf42324661ed6c962bd13bcc285a287aa9e68
momo <oaithel@yahoo.com>
2026-09-24 13:34:26 -0400

    9/24/2026

---
+++ src/com/mac190/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.mac190.calculator;
+/*
+Write a Java program that accepts an input in the form double operator double
+23.4 + 45.6
+and performs the operation and displays the result. The operator could be
++ - / or *
+ */
+public class Calculator {
+}

85c9563fa80fd9059cde7645c2fb5b8d9ddc8b46
momo <oaithel@yahoo.com>
2026-09-24 14:22:04 -0400

    9/24/2026

--- src/com/mac190/calculator/Calculator.java
+++ src/com/mac190/calculator/Calculator.java
@@ -1,4 +1,7 @@
 package com.mac190.calculator;
+
+import java.util.Scanner;
+
 /*
 Write a Java program that accepts an input in the form double operator double
 23.4 + 45.6
@@ -6,4 +9,28 @@ and performs the operation and displays the result. The operator could be
 + - / or *
  */
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an expression in the form 23 + 34");
+        double num1 = sc.nextDouble();
+        char op = sc.next().charAt(0);
+        double num2 = sc.nextDouble();
+
+        if(op == '+'){
+            System.out.println(num1 +  " + " + num2 + " = " + (num1 + num2));
+        }else if(op == '-'){
+            System.out.println(num1 +  " - " + num2 + " = " + (num1 - num2));
+        }else if (op == '*'){
+            System.out.println(num1 +  " * " + num2 + " = " + (num1 * num2));
+        }else if(op == '/'){
+            if(num2 == 0){
+                System.out.println("Trying to divide by 0!");
+            }else{
+                System.out.println(num1 +  " / " + num2 + " = " + (num1 / num2));
+            }
+        }else{
+            System.out.println("Invalid operation");
+        }
+
+    }
 }
